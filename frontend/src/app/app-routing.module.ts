1ad833bde3b4662b08e42a7447c4685091f53b6a
David Knox <dakdave26@gmail.com>
2020-10-08 19:37:19 -0400

    Added routing homepage and navbar skeletons

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './views/homepage/homepage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component: HomepageComponent,
+    path: 'index',
+    pathMatch: 'full'
+  },
+  {
+    redirectTo: 'index',
+    path: '',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { HomepageComponent } from './views/homepage/homepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,

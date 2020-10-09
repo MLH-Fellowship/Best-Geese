39970f0a032b4982542d90ce89f063c1228b57c8
shayanriyaz <shayanariyaz@gmail.com>
2020-10-09 11:18:56 -0700

    Fixed models.py (some values will not be unique)

--- backend/database/models.py
+++ backend/database/models.py
@@ -1,12 +1,17 @@
 from .db import db
-
+from flask.ext.bycrypt import Bycrypt
 
 class Question(db.Document):
-    tag = db.StringField(required=True,unique=True)
-    difficulty = db.StringField(required=True,unique=True)
+    tag = db.StringField(required=True)
+    difficulty = db.StringField(required=True)
     question = db.StringField(required=True,unique=True)
-    option_1 = db.StringField(required=True,unique=True)
-    option_2 = db.StringField(required=True,unique=True)
-    option_3 = db.StringField(required=True,unique=True)
-    option_4 = db.StringField(required=True,unique=True)
-    correct_answer = db.StringField(required=True,unique=True)
+    option_1 = db.StringField(required=True)
+    option_2 = db.StringField(required=True)
+    option_3 = db.StringField(required=True)
+    option_4 = db.StringField(required=True)
+    correct_answer = db.StringField(required=True)
+
+
+class User(db.Document):
+    email = db.EmailField(required=True,unique=True)
+    password = db.StringField(required=True,min_length=6)
